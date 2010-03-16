3b24a1519b7166c6c6f0c10690e80b8760406686
Takaaki Kato <takaaki.kato@gmail.com>
2010-03-16 17:51:12 +0900

    Added a way to show/hide certain vendors with checkboxes. Fixed a bug where unnecessary 0 values show up.

--- public/application.js
+++ public/application.js
@@ -49,6 +49,8 @@ $(document).ready(function() {
     $(this).focus().select();
   });
   
+  $(':checkbox').attr("checked", "true");
+  
   var get_code = function(vendor) {
     short_code = "";
     tl_code = "";
@@ -56,19 +58,21 @@ $(document).ready(function() {
     bl_code = "";
     br_code = "";
     
+    var frequencies = {};
+    var max_index; // Most frequest value
+    var max_value = 0; // How many times the value repeats in each array.
+    // var max_value = 0
+    var min_value = 0;
+    
     if (unique_radius_values.length === 1) { // e.g., [2, 2, 2, 2]
       if (unique_radius_values[0] !== 0) {
         short_code += props[vendor]["short"] + ": " + unique_radius_values[0] + "px;\n";
-      };
+      }
 
     } else if (unique_radius_values.length === 2) {
-      var frequencies = {};
-      var max_index; // Most frequest value
-      var max_value = 0; // How many times the value repeats in each array.
-
       $.each(all_radius_values, function(index, value) {
         if (frequencies[value] !== undefined) {
-          frequencies[value]++;
+          frequencies[value] = frequencies[value] + 1;
         } else {
           frequencies[value] = 1;
         }
@@ -82,18 +86,31 @@ $(document).ready(function() {
       });
       
       if (max_value === 3) { // e.g., [7, 7, 3, 7]
-        short_code += props[vendor]["short"] + ": " + max_index + "px;\n";
-        if (tl_value != max_index) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
-        if (tr_value != max_index) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
-        if (bl_value != max_index) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
-        if (br_value != max_index) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
+        max_value = Math.max.apply(Math, all_radius_values);
+        min_value = Math.min.apply(Math, all_radius_values);
+        if (min_value == 0) {
+          if (tl_value != 0) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
+          if (tr_value != 0) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
+          if (bl_value != 0) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
+          if (br_value != 0) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
+          
+        } else {
+          short_code += props[vendor]["short"] + ": " + max_index + "px;\n";
+          if (tl_value != max_index) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
+          if (tr_value != max_index) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
+          if (bl_value != max_index) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
+          if (br_value != max_index) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
+          
+        }
+        
       } else if (max_value === 2) { // e.g., [4, 4, 2, 2]
-        var min_value = Math.min.apply(Math, all_radius_values);
-        short_code += props[vendor]["short"] + ": " + min_value + "px;\n";
-        if (tl_value != min_value) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
-        if (tr_value != min_value) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
-        if (bl_value != min_value) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
-        if (br_value != min_value) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
+        max_value = Math.max.apply(Math, all_radius_values);
+        min_value = Math.min.apply(Math, all_radius_values);
+        if (min_value != 0) {short_code += props[vendor]["short"] + ": " + max_value + "px;\n";}
+        if (tl_value == max_value) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
+        if (tr_value == max_value) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
+        if (bl_value == max_value) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
+        if (br_value == max_value) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
      }
      
      
@@ -101,13 +118,10 @@ $(document).ready(function() {
       /*
         Code duplication warning!
       */
-      var frequencies = {};
-      var max_index;
-      var max_value = 0;
 
       $.each(all_radius_values, function(index, value) {
         if (frequencies[value] !== undefined) {
-          frequencies[value]++;
+          frequencies[value] = frequencies[value] + 1;
         } else {
           frequencies[value] = 1;
         }
@@ -124,17 +138,17 @@ $(document).ready(function() {
       */
       
       var min_value = Math.min.apply(Math, all_radius_values);
-      short_code += props[vendor]["short"] + ": " + min_value + "px;\n";
+      if (min_value != 0) { short_code += props[vendor]["short"] + ": " + min_value + "px;\n"; }
       if (tl_value != min_value) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
       if (tr_value != min_value) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
       if (bl_value != min_value) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
       if (br_value != min_value) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
     } else { // unique_radius_values.length === 4
       // Each value is different. e.g., [7, 1, 8, 4]
-      tl_code += props[vendor].topleft + ": " + tl_value + "px;\n";
-      tr_code += props[vendor].topright + ": " + tr_value + "px;\n";
-      bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n";
-      br_code += props[vendor].bottomright + ": " + br_value + "px;\n";
+      if (tl_value != 0 ) { tl_code += props[vendor].topleft + ": " + tl_value + "px;\n"; }
+      if (tr_value != 0 ) { tr_code += props[vendor].topright + ": " + tr_value + "px;\n"; }
+      if (bl_value != 0 ) { bl_code += props[vendor].bottomleft + ": " + bl_value + "px;\n"; }
+      if (br_value != 0 ) { br_code += props[vendor].bottomright + ": " + br_value + "px;\n"; }
     }
     
     var complete_code = short_code + tl_code + tr_code + bl_code + br_code;
@@ -148,22 +162,34 @@ $(document).ready(function() {
     bl_value = $('#slider_bottomleft').slider('option', 'value');
     br_value = $('#slider_bottomright').slider('option', 'value');
   };
+  var update = function(){
+    get_values_from_sliders();
+    all_radius_values = [tl_value, tr_value, bl_value, br_value];
+    unique_radius_values = all_radius_values.unique();
+    var webkit, moz, css3;
+    var code = "";
+    if ($("#webkit").attr("checked")) { webkit = get_code("webkit"); }
+    if ($("#moz").attr("checked")) { moz = get_code("moz"); }
+    if ($("#css3").attr("checked")) { css3 = get_code("css3"); }
+    
+    if (webkit) { code += webkit; }
+    if (moz) { code += moz; }
+    if (css3) { code += css3; }
+    
+    $('#box').attr("style", code);
+    
+    $('#copiable').text(code);
+  };
   
   $(".slider").slider({
     animate: true,
     change: function(event, ui) {
-      get_values_from_sliders();
-      all_radius_values = [tl_value, tr_value, bl_value, br_value];
-      unique_radius_values = all_radius_values.unique();
-      
-      var webkit = get_code("webkit");
-      var moz = get_code("moz");
-      var css3 = get_code("css3");
-      var code = css3 + webkit + moz;
-      $('#box').attr("style", code);
-      
-      $('#copiable').text(code);
+      update();
       $(this).next().text(ui.value);
     }
+  });
+  
+  $(":checkbox").click(function() {
+      update();    
   });
 });
